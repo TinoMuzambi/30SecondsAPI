526f4897e4f0556e25ec80148f1394d5acf0db60
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-18 19:17:53 +0200

    Sorts out query params.

--- pages/api/v1/card/index.ts
+++ pages/api/v1/card/index.ts
@@ -3,14 +3,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const {
 		method,
-		query: { category },
+		query: { category, itemsPerCard, difficulty },
 	} = req;
 
 	switch (method) {
 		case "GET":
+			const categoryParam = category as string | "all";
+			const itemsPerCardParam = Number.parseInt(itemsPerCard as string) | 5;
+			const difficultyParam = difficulty as
+				| ("easy" | "medium" | "hard")
+				| "default";
 			res.status(200).json({
 				success: "true",
-				data: { cat: category },
+				data: { categoryParam, itemsPerCardParam, difficultyParam },
 			});
 			break;
 		default:
