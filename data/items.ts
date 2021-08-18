9cfb44c74db8296a3a22ecca50387bd80f434abd
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-18 20:14:07 +0200

    Adds some items.

--- data/items.ts
+++ data/items.ts
@@ -8,6 +8,20 @@ const items: Item[] = [
 		content: "Maserati",
 		clue: "Fork Logo",
 	},
+	{
+		id: 1000,
+		category: [CATEGORIES.cars],
+		difficulty: DIFFICULTIES.default,
+		content: "Ferrari",
+		clue: "Red horse",
+	},
+	{
+		id: 1000,
+		category: [CATEGORIES.cars],
+		difficulty: DIFFICULTIES.default,
+		content: "Bugatti",
+		clue: "Very fast",
+	},
 ];
 
 export default items;
