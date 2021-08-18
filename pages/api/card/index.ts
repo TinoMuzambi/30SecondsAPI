8d44dbd21c6c30430c5cc21f968bd106a656493a
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-18 19:03:35 +0200

    Adds test route.

--- pages/api/card/index.ts
+++ pages/api/card/index.ts
@@ -1,13 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const { method } = req;
+	const {
+		method,
+		query: { category },
+	} = req;
 
 	switch (method) {
 		case "GET":
 			res.status(200).json({
 				success: "true",
-				data: "Hello! Welcome to the 30 Seconds API!",
+				data: { category },
 			});
 
 		default:
