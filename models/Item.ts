63c8c763763fb178ed7f1ceadf23e1c41c470173
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-19 14:49:28 +0200

    Adds item schema template.

---
+++ models/Item.ts
@@ -0,0 +1,69 @@
+import mongoose, { Schema } from "mongoose";
+
+import { Item } from "../interfaces";
+
+const ItemSchema: Schema = new mongoose.Schema(
+	{
+		id: {
+			type: String,
+			required: [true, "Comment needs an ID."],
+			trim: true,
+		},
+		videoId: {
+			type: String,
+			required: [true, "Comment needs a video ID."],
+			trim: true,
+		},
+		authorId: {
+			type: String,
+			required: [true, "Comment needs an author ID"],
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: [true, "Comment needs an email"],
+			trim: true,
+		},
+		comment: {
+			type: String,
+			required: [true, "Comment needs comment text."],
+		},
+		name: {
+			type: String,
+			required: [true, "Comment needs a name"],
+			trim: true,
+		},
+		image: {
+			type: String,
+			trim: true,
+			default:
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSisT6Nb6XIXyX7kQ9XmEMID6eSxl4mQ8E0vXbwc77pJqhZYUUdU13h7VRlt4rZqOgg5Yc&usqp=CAU",
+		},
+		upvotes: {
+			type: Number,
+			default: 0,
+		},
+		downvotes: {
+			type: Number,
+			default: 0,
+		},
+		mention: {
+			type: String || null,
+			default: null,
+		},
+		replies: {
+			type: [this],
+		},
+		edited: {
+			type: Boolean,
+			required: [true, "Comment needs edited value."],
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+		strict: true,
+	}
+);
+
+export default mongoose.models.Item || mongoose.model<Item>("Item", ItemSchema);
