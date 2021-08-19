35983d839d7403b64908502804806152d342a25c
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-19 11:30:46 +0200

    Implements getting and returning card.

--- pages/api/v1/card/index.ts
+++ pages/api/v1/card/index.ts
@@ -1,8 +1,45 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { v4 as uuidv4 } from "uuid";
+
 import cards from "../../../../data/cards";
-import { Card } from "../../../../interfaces";
+import items from "../../../../data/items";
+import { Card, CATEGORIES, DIFFICULTIES, Item } from "../../../../interfaces";
+import { intersection } from "../../../../utils";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+	// Get items that match number of items and category.
+	const getItems = (noItems: number, categories: CATEGORIES[]): Item[] => {
+		let cardItems: Item[] = [];
+
+		while (cardItems.length < noItems) {
+			const rand = Math.floor(Math.random() * items.length);
+			const currItem = items[rand];
+			if (
+				intersection(categories, currItem.category) &&
+				difficulty.includes(currItem.difficulty)
+			) {
+				cardItems.push(currItem);
+			}
+		}
+
+		return cardItems;
+	};
+	const getCard = (
+		noItems: number,
+		categories: CATEGORIES[],
+		difficulty: DIFFICULTIES[]
+	): Card => {
+		const cardItems = getItems(noItems, categories);
+		const card: Card = {
+			id: uuidv4(),
+			category: categories,
+			difficulty: difficulty,
+			items: cardItems,
+			noItems: cardItems.length,
+		};
+
+		return card;
+	};
 	const {
 		method,
 		query: { category, noItemsPerCard, difficulty },
@@ -11,17 +48,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	switch (method) {
 		case "GET":
 			// Initialise query params and check if they exist
-			const categoryParam = (category as string) ? category : "all";
-			const noItemsPerCardParam = (noItemsPerCard as string)
+			let categoryParam = (category as string) ? category : "all";
+			let noItemsPerCardParam = (noItemsPerCard as string)
 				? Number.parseInt(noItemsPerCard as string)
 				: 5;
-			const difficultyParam = (difficulty as string) ? difficulty : "default";
+			let difficultyParam = (difficulty as string) ? difficulty : "default";
 
-			const cardsData: Card[] = [...cards];
+			const finalCategoryParam: string[] =
+				typeof categoryParam === "string" ? [categoryParam] : categoryParam;
+			const finalDifficultyParam: string[] =
+				typeof difficultyParam === "string"
+					? [difficultyParam]
+					: difficultyParam;
+
+			const card: Card = getCard(
+				noItemsPerCardParam,
+				finalCategoryParam,
+				finalDifficultyParam
+			);
 
 			res.status(200).json({
 				success: "true",
-				data: { cardsData },
+				data: { card },
 			});
 			break;
 		default:
